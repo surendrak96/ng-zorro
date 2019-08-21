37059b5a76fe2b64eb1e9b7bed2f256d111243e5
Surendra Reddy <imsurendra99@gmail.com>
2019-08-21 23:27:15 +0400

    Date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ng-Zorro';
+
+  endOpen = false;
+  endValue;
+  startValue: Date | null = new Date();
+
+  openCalender(): void {
+    this.endOpen = !this.endOpen;
+  }
+
+  disabledEndDate = (endValue: Date): boolean => {
+    if (!endValue || !this.startValue) {
+      return false;
+    }
+    return endValue.getTime() <= this.startValue.getTime();
+  }
+
+  handleEndOpenChange(open: boolean): void {
+    console.log(open);
+    this.endOpen = open;
+  }
+  onEndChange(date: Date): void {
+    this.endValue = date;
+
+  }
 }
